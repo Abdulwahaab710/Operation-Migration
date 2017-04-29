503cdc52279e87c1649911276bd61a309803600e
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-29 17:17:06 -0400

    if request is not POST raise a 404 error

--- backend/birdWatchers/views.py
+++ backend/birdWatchers/views.py
@@ -1,7 +1,7 @@
 from birdWatchers import app
 import tensorflow as tf
 import os
-from flask import request, jsonify
+from flask import request, jsonify, abort
 import base64
 # from birdWatchers.Inception.inception import download
 from birdWatchers.inception import Inception
@@ -12,23 +12,24 @@ def main():
     return '<h1>hello world!</h1>'
 
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['POST', 'GET'])
 def search():
-    print(request.json)
+    print(request.method)
     print(tf.__version__)
-    jsonRequest = request.json
-    base64ToImg(jsonRequest['image'])
-    image_path = os.path.join('temp.jpg')
-    # Load the Inception model so it is ready for classifying images.
-    model = Inception()
-    pred = model.classify(image_path=image_path)
-    # Print the scores and names for the top-10 predictions.
-    l = model.print_scores(pred=pred, k=10)
-    print(l)
-    # Close the TensorFlow session.
-    model.close()
-    response = {"top3":l[0:3]}
-    return jsonify( response )
+    if request.method == 'POST':
+        jsonRequest = request.json
+        base64ToImg(jsonRequest['image'])
+        image_path = os.path.join('temp.jpg')
+        # Load the Inception model so it is ready for classifying images.
+        model = Inception()
+        pred = model.classify(image_path=image_path)
+        # Print the scores and names for the top-10 predictions.
+        l = model.print_scores(pred=pred, k=10)
+        # Close the TensorFlow session.
+        model.close()
+        response = {"top3":l[0:3]}
+        return jsonify( response )
+    return abort(404)
 
 
 def base64ToImg(img_data):
