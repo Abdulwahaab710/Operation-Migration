ac69ee88318950adc59e092b6d3f2994027c01fe
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-29 16:25:58 -0400

    handling base64

--- backend/birdWatchers/views.py
+++ backend/birdWatchers/views.py
@@ -1,7 +1,8 @@
 from birdWatchers import app
 import tensorflow as tf
 import os
-# from flask import render_template
+from flask import request
+import base64
 # from birdWatchers.Inception.inception import download
 from birdWatchers.inception import Inception
 
@@ -12,11 +13,16 @@ def main():
 
 
 @app.route('/search', methods=['POST'])
-def search():
-    print(tf.__version__)
+def test():
+    # print(request.json)
+    jsonRequest = request.json
+    base64ToImg(jsonRequest['image'])
+    return 'hello'
 
-    # Download Inception model if not already done.
-    # maybe_download()
+
+def search():
+    print(request.json)
+    print(tf.__version__)
 
     # data_dir = "birdWatchers/inception"
     # imagePath = 'cropped_panda.jpg'
@@ -25,7 +31,6 @@ def search():
     # Load the Inception model so it is ready for classifying images.
     model = Inception()
     pred = model.classify(image_path=image_path)
-
     # Print the scores and names for the top-10 predictions.
     l = model.print_scores(pred=pred, k=10)
 
@@ -41,6 +46,10 @@ def search():
 
 
 def base64ToImg(img_data):
-    img = open('temp.jpg', 'wb')
-    img.write(img_data.decode('base64'))
-    img.close()
+    img_data = str.encode(img_data)
+    with open('temp.jpg', 'wb') as img:
+        img.write(base64.decodestring(img_data))
+    # img_data = str.encode(img_data)
+    # img = open('temp.jpg', 'wb')
+    # img.write(img_data.decode('base64'))
+    # img.close()
