c6a566b1e10558efe0ec19408f87ecd405627326
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-29 23:45:10 -0400

    adding missing imports

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,13 @@
 import dotenv
+from os.path import join, dirname
+
 DOTENV_PATH = join(dirname(__file__), '.env')
+dotenv.load(DOTENV_PATH)
+
 USERNAME = dotenv.get('USERNAME')
 PASSWORD = dotenv.get('PASSWORD')
 DATABASE = 'birdWatchers'
+
 SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@localhost/{}'.formt(
     USERNAME,
     PASSWORD,
