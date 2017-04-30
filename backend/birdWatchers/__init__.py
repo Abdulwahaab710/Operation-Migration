7ec9aeecc1d145f05d9f03fff1276dd42ca1f351
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-29 23:50:29 -0400

    integrating SQLAlchemy to the flask app

--- backend/birdWatchers/__init__.py
+++ backend/birdWatchers/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+# Create an Instance of Flask
 app = Flask(__name__)
-
-import birdWatchers.views
+# Include config from config.py
+app.config.from_object('config')
+app.secret_key = 'some_secret'
+# Create an instance of SQLAclhemy
+db = SQLAlchemy(app)
+from birdWatchers import views, models
