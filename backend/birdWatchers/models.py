8c53946872bf51d65a47cc1043cc18ee414e9796
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-30 00:22:19 -0400

    added a new model SpottedBird

--- backend/birdWatchers/models.py
+++ backend/birdWatchers/models.py
@@ -10,4 +10,17 @@ class Bird(db.Model):
     def __init__(self, bird_name):
         self.bird_name = bird_name
 
-
+class SpottedBird(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gps_lat = db.Column(db.string(db.Float))
+    gps_long = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime)
+    bird_id = db.Column(
+        db.Integer,
+        db.ForeignKey('bird.id')
+    )
+    def __init__(self, gps_lat, gps_long, timestamp, bird_id):
+        self.gps_lat = gps_lat
+        self.gps_long = gps_long
+        self.timestamp = timestamp
+        self.bird_id = bird_id
