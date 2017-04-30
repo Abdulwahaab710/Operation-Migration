7fd9ae8c9c3c8236590ebfbb23d7284f8cc5950d
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-30 00:25:04 -0400

    fixing coding style

--- backend/birdWatchers/models.py
+++ backend/birdWatchers/models.py
@@ -1,4 +1,6 @@
 from birdWatchers import db
+
+
 class Bird(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bird_name = db.Column(db.String(255))
@@ -7,8 +9,10 @@ class Bird(db.Model):
         backref='spotted_bird',
         lazy='dynamic'
     )
+
     def __init__(self, bird_name):
         self.bird_name = bird_name
+
 
 class SpottedBird(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +23,7 @@ class SpottedBird(db.Model):
         db.Integer,
         db.ForeignKey('bird.id')
     )
+
     def __init__(self, gps_lat, gps_long, timestamp, bird_id):
         self.gps_lat = gps_lat
         self.gps_long = gps_long
