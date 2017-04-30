e7526d6f1207cf0bec09dec4797d8c674cb33cb6
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-30 12:23:08 -0400

    added a new column to the spottedBird model

--- backend/birdWatchers/models.py
+++ backend/birdWatchers/models.py
@@ -19,13 +19,15 @@ class SpottedBird(db.Model):
     gps_lat = db.Column(db.Float)
     gps_long = db.Column(db.Float)
     timestamp = db.Column(db.DateTime)
+    image = db.Column(db.String(255))
     bird_id = db.Column(
         db.Integer,
         db.ForeignKey('bird.id')
     )
 
-    def __init__(self, gps_lat, gps_long, timestamp, bird_id):
+    def __init__(self, gps_lat, gps_long, timestamp, bird_id, image):
         self.gps_lat = gps_lat
         self.gps_long = gps_long
         self.timestamp = timestamp
         self.bird_id = bird_id
+        self.image = image
